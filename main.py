b7895f6d70bf642d0d371269ee2a68d8a8e1e943
DevCielo <136787904+DevCielo@users.noreply.github.com>
2024-12-06 16:26:10 +1000

    Finished the implementation for the data validation pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig, DataValidationConfig
 from networksecurity.logging.logger import logging
 import sys
 
@@ -11,8 +12,17 @@ if __name__ == "__main__":
         training_pipeline_config = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-        logging.info("Initate the data ingestion")
+        logging.info("Initiate the data ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+
+        data_validation_config= DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)

51c07b8476936fcea81c26784be70bfb10e0dd74
DevCielo <136787904+DevCielo@users.noreply.github.com>
2024-12-07 07:02:56 +1000

    Created a model training pipeline and performed model training, evaluation and hyperparameter tuning

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.entity.config_entity import TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
@@ -34,6 +36,13 @@ if __name__ == "__main__":
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("Data transformation completed")
+
+        # Model trainer
+        logging.info("Model trainer started")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model trainer completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
